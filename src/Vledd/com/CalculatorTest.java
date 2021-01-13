d76781c6a99ef1ef04e1a5aa1911244be1dbfda7
kyba <kyba@ozon.ru>
2021-01-14 02:32:29 +0300

    add tests validate

--- src/Vledd/com/CalculatorTest.java
+++ src/Vledd/com/CalculatorTest.java
@@ -1,6 +1,5 @@
 package Vledd.com;
 
-import Vledd.com.Calculator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -19,6 +18,18 @@ class CalculatorTest {
         var c = new Calculator(primer);
         c.calc();
         Assertions.assertEquals(otvet, c.getResult());
+    }
+
+    @ParameterizedTest
+    @MethodSource("DataValidate")
+    public void TestValidate(String primer, boolean otvet) throws Exception {
+        Assertions.assertEquals(otvet, !new Calculator(primer).validate(primer).existError);
+    }
+
+    public static List<Object[]> DataValidate() {
+        return Arrays.asList(new Object[][]{
+                {"0+0", true},
+        });
     }
 
     public static List<Object[]> Data() {
